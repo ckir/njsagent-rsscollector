5d37935922a7c6f91da355921b634f1dbf5fa69c
ckir <ckir1508@hotmal.com>
2016-01-07 16:55:45 +0200

    Initial commit

--- php/FeedPoll.php
+++ php/FeedPoll.php
@@ -3,4 +3,5 @@ require_once ("Startup.php");
 
 $logger->logInfo ( __FILE__ . " started at " . date(DATE_ATOM));
 $logger->logInfo ("RSSCOLLECTOR_PGSQL=" . getenv("RSSCOLLECTOR_PGSQL"));
+$logger->logInfo ("RSSCOLLECTOR_MYSQL=" . getenv("RSSCOLLECTOR_MySQL"));
 sleep(5);
