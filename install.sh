4719d4097d07a6d5fae3220614b62df040aca3fe
ckir <ckir1508@hotmal.com>
2016-01-16 00:41:19 +0200

    Initial commit

--- install.sh
+++ install.sh
@@ -9,5 +9,7 @@ composer install
 cd ..
 
 #Put binaries to a path accesible folder
-ln -s $NJSAGENT_APPROOT/workers/njsagent-rsscollector/node_modules/phantomjs/bin/phantomjs /app/.heroku/node/bin
-ln -s $NJSAGENT_APPROOT/workers/njsagent-rsscollector/node_modules/casperjs/bin/casperjs /app/.heroku/node/bin
+if [ -d "/app/.heroku/node/bin" ]; then
+  ln -s $NJSAGENT_APPROOT/workers/njsagent-rsscollector/node_modules/phantomjs/bin/phantomjs /app/.heroku/node/bin
+  ln -s $NJSAGENT_APPROOT/workers/njsagent-rsscollector/node_modules/casperjs/bin/casperjs /app/.heroku/node/bin
+fi
