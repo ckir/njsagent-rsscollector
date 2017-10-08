dd7a5f57ce5c62c559c7ef0072567d3150cb0b97
Costas Kirgoussios <ckir1508@hotmail.com>
2017-10-08 20:31:43 +0300

    Initial commit

--- npm_reinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-php npmreinstall.php
-npm remove socket.io --save
-npm install socket.io@0.9.17 --save
-
--- project_upload.sh
+++ project_upload.sh
@@ -5,7 +5,7 @@ set -e
 set -o pipefail
 
 sudo rm -Rf /var/www/GITHUB/njsagent-rsscollector/php/Rss/Feed/Reader/cache/zfcache*
-git add .
+git add --all .
 git commit -m "Initial commit"
 git push
 
