62b97c4ea07547cb50b28819930cc60e800ace0d
Costas Kirgoussios <ckir1508@hotmail.com>
2017-10-10 23:36:58 +0300

    Initial commit

--- install.sh
+++ install.sh
@@ -3,12 +3,10 @@
 set -e
 set -o pipefail
 
+exit 0
+
 cd php
 composer install
 cd ..
 
-#Put binaries to a path accesible folder
-if [ -d "/app/.heroku/node/bin" ]; then
-  ln -s $NJSAGENT_APPROOT/node_apps/node_modules/phantomjs-prebuilt/bin/phantomjs /app/.heroku/node/bin
-  ln -s $NJSAGENT_APPROOT/node_apps/node_modules/casperjs/bin/casperjs /app/.heroku/node/bin
-fi
+
