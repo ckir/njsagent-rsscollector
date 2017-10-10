ba320d8a8e1f38c78785567c18333a672c8134ba
Costas Kirgoussios <ckir1508@hotmail.com>
2017-10-10 18:33:50 +0300

    Initial commit

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@
 set -e
 set -o pipefail
 
+exit 0
 cd php
 composer install
 cd ..
 
 #Put binaries to a path accesible folder
-if [ -d "/app/.heroku/node/bin" ]; then
-  ln -s $NJSAGENT_APPROOT/workers/njsagent-rsscollector/node_modules/phantomjs/bin/phantomjs /app/.heroku/node/bin
-  ln -s $NJSAGENT_APPROOT/workers/njsagent-rsscollector/node_modules/casperjs/bin/casperjs /app/.heroku/node/bin
-fi
+# if [ -d "/app/.heroku/node/bin" ]; then
+#   ln -s $NJSAGENT_APPROOT/workers/njsagent-rsscollector/node_modules/phantomjs/bin/phantomjs /app/.heroku/node/bin
+#   ln -s $NJSAGENT_APPROOT/workers/njsagent-rsscollector/node_modules/casperjs/bin/casperjs /app/.heroku/node/bin
+# fi
